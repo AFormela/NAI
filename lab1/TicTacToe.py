8b330870d278c0288631b357a3e754d57b003520
AFormela <aleksandra_formela@vp.pl>
2020-10-30 17:49:02 +0100

    TicTacToe

--- lab1/TicTacToe.py
+++ lab1/TicTacToe.py
@@ -1,4 +1,4 @@
-#Źródło problemu: https://www.codingame.com/ide/puzzle/power-of-thor-episode-1
+#Źródło problemu: https://www.codingame.com/ide/puzzle/tictactoe
 #Twórca rozwiązania: Aleksandra Formela, s17402
 
 import sys
